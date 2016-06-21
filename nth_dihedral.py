2658884376e8f11b13b5f673518787fdd833f5ea
Curt Waltmann <waltmann@iastate.edu>
2016-06-21 11:16:46 -0500

    replace nth dihedral

--- nth_dihedral.py
+++ nth_dihedral.py
@@ -57,11 +57,23 @@ def gminus_nth_dihedral(inputfile,n, l):
             gminus+=1
     return gminus/len(dhs)
 
-for i in range(1,10):
-    print(str(trans_nth_dihedral('atoms2.dump.0004599999.xml',i,13)))
+#print(all_nth_dihedral())
+def average_trans(inputfile):
+    numtrans=0.0
+    numgplus=0.0
+    numgminus=0.0    
+    trans_cut=1.0472
+    d=t.all_dihedral_angles(inputfile)
+    for x in range(1,len(d)):
+        if(abs(d[x])<trans_cut):
+            numtrans+=1;
+        elif(d[x]<-1.0*trans_cut):
+            numgminus+=1
+        else:
+            numgplus+=1
+    return numtrans/float(len(d)-1)
 
-
-
+#print(average_trans('atoms.dump.0001541665.xml'))
 #############
 ##this figures out the average percent trans or gauce over a number of files throughout a
 ####simultaion for either the nth or all dihedrals
@@ -101,4 +113,25 @@ def trans_average_of_files(nfiles,file1,file2, nth, l):
     return avefile
 
 
-#print(trans_average_of_files(16,'atoms.dump.0004600000.xml','atoms.dump.0004725000.xml',0, 13))
+#print(trans_average_of_files(19,'atoms.dump.0006541657.xml','atoms.dump.0006749990.xml',0, 13))
+
+def trans_v_timestep(nfiles,file1,file2, nth, l):
+    file1=file1[11:-4]
+    file2=file2[11:-4]
+    out=np.array([[0.0,0.0]])
+    for c in range(0,nfiles):
+        file0=(int(file1)+(int(file2)-int(file1))*c)
+        toopen='atoms.dump.'
+        for v in range(0,10-len(str(file0))):
+            toopen=toopen + '0'
+        toopen=toopen + str(file0) + '.xml'
+        #print(toopen)
+        if(nth==0):
+            out=np.append(out,[[file0,average_trans(toopen)]],axis=0)
+        else:
+            out=np.append(out,trans_nth_dihedral(toopen,nth,l),axis=0)
+        #out=np.delete(out,0,0)
+    return out
+x=trans_v_timestep(193,'atoms.dump.0000500000.xml','atoms.dump.0000552083.xml',0, 13)
+for i in range(1,len(x)):
+    print(str(x[i][0])+ ' '+ str(x[i][1]))
